f3f72a3e3756eb59daf40c31015944df55113c6f
juan <alva.juan79@gmail.com>
2016-02-25 21:52:14 -0800

    Building menu. #1. #5

---
+++ js/default.js
@@ -0,0 +1,13 @@
+var paletaSpanishButton = document.getElementById('paletaButton');
+$(document).click(function() {
+  $('#toggle').toggle('slide');
+});
+
+paletaSpanishButton.addEventListener('click', function() {
+
+  for (var i = 0; i < paletasSpanish.length; i++) {
+    
+  }
+}, false);
+
+console.log(paletasSpanish);
---
+++ js/product.js
@@ -0,0 +1,10 @@
+var paletasSpanish = [
+  "Fresa",
+  "Vanilla", 
+  "Nuez",
+  "Chicle",
+  "Arroz",
+  "Fresa con Crema",
+  "Nance", 
+  "Cookies and Cream",
+]

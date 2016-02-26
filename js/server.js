01ecfc667efac310fe2585dda384876b1c76e73d
Juan Alvarado <alva.juan79@gmail.com>
2016-02-25 17:41:51 -0800

    Paleteria Limon server.js file

--- js/server.js
+++ js/server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
-app.use(express.static('./'));
+app.get('/public/spanish.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/spanish.html'));
+});
+
+app.get('/public/english.html', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/english.html'));
+});
+
+app.get('../', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port);

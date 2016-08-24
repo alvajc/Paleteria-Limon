39aad01aa89ea0da4034047698e1d2b3cb31ca71
Juan Alvarado <alva.juan79@gmail.com>
2016-08-24 16:52:33 -0700

    Added routes for new pages

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ app.get('/public/menuEnglish.html', function(req, res) {
   res.render(__dirname + '/public/menuEnglish.jade');
 });
 
+app.get('/public/location.html', function(req, res) {
+  res.render(__dirname + '/public/location.jade');
+});
+
+app.get('/public/contact.html', function(req, res) {
+  res.render(__dirname + '/public/contact.jade');
+});
+
+app.get('/public/followUs.html', function(req, res) {
+  res.render(__dirname + '/public/followUs.jade');
+});
+
 app.get('/', function(req, res) {
   res.render(__dirname + '/index.jade');
 });

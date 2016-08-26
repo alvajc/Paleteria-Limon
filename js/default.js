882b45b748a5d4f94dc40a1ee6388e34091c2bc6
juan <alva.juan79@gmail.com>
2016-08-25 22:33:45 -0700

    Google map displayed

--- js/default.js
+++ js/default.js
@@ -1,9 +1,28 @@
-var map = document.getElementById('storeMap');
-var storeLat = 33.9836832;
-var storeLong = -118.1856951;
-var storeLatLong = {lat: storeLat, lng: storeLong};
-map = new google.maps.Map(map, {
-center: storeLatLong,
-scrollwheel: false,
-zoom: 18
-});
+function initMap() {
+  var myLatLng = {lat: 33.983181, lng: -118.185696};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng,
+    scrollwheel: false,
+    zoom: 16
+  });
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng,
+    title: 'Paleteria Limon'
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+

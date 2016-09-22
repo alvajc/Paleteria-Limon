b86e60e0e267fa5648b826db409090aa45181794
juan <alva.juan79@gmail.com>
2016-09-21 22:45:25 -0700

    Added jQuery for smooth scrolling and div show on scroll

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
+// Google map function
+
 function initMap() {
   var myLatLng = {lat: 33.983181, lng: -118.185696};
 
@@ -19,6 +21,7 @@ function initMap() {
   });
 };
 
+// jQuery for smooth scrolling
 
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -36,11 +39,45 @@ $(function() {
 });
 
 
+// jQuery for div show on scroll
 
-
-
-
-
-
-
+if ($(window).width() > 991) {
+  $(document).ready(function() {
+  
+  /* Hide all elements outside the visible window */
+  $('body *').each( function(){
+     
+    var top_of_object = $(this).position().top;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+     
+    if( bottom_of_window < top_of_object  ){
+    
+      $(this).addClass('hideme').css({'opacity':'0'});
+      
+    }
+    
+  });
+  
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+    
+    /* Check the location of the desired elements */
+    $('.hideme').each( function(i){
+      
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+      
+        if( bottom_of_window > ( bottom_of_object + 20 )  ){
+        
+          $(this).removeClass('hideme').animate({'opacity':'1'},500);
+          
+        }
+      
+    }); 
+  
+  });
+  
+ });
+}
 

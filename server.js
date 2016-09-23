56aa4d8d201a177e132bcaa5f5bb4b8394ad04aa
juan <alva.juan79@gmail.com>
2016-09-23 00:41:34 -0700

    Added google analytics npm module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
-
+var ua = require('universal-analytics');
+var visitor = ua('UA-81977400-2')
 
 app.get('/public/spanish.html', function(req, res) {
   res.render(__dirname + '/public/spanish.jade');
@@ -18,5 +19,8 @@ app.use(express.static('./'));
 
 app.set('view engine', 'jade');
 
+visitor.pageview('/', function (err) {
+  
+})
 var port = process.env.PORT || 3000;
 app.listen(port);

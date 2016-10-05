e95a63f1c4862595c4609d220cf139d0fbd1d748
Juan Alvarado <alva.juan79@gmail.com>
2016-10-05 15:05:14 -0700

    Added controller for navbar

--- js/controllers.js
+++ js/controllers.js
@@ -1,2 +1,7 @@
 var PaleteriaLimonApp = angular.module('myApp', []);
 
+PaleteriaLimonApp.controller('navController', ['$scope', function($scope) {
+  $scope.menu = "Menu";
+  $scope.contact = "Contact Us";
+  $scope.follow = "Follow Us";
+}]);
